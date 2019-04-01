dc2797c423688eadebaaf43cabf0821c691c6b13
StartAutomating <james.brundage@start-automating.com>
2019-04-01 14:19:11 -0700

    Fixing example by making it a .ps1 benchmark

---
+++ PowerShellPerformance/Ways_To_Hash_A_File.benchmark.ps1
@@ -0,0 +1,14 @@
+﻿#require -Module Benchpress
+$myScript = $MyInvocation.MyCommand.ScriptBlock.File
+
+bench @{
+    DotNet = { 
+        [BitConverter]::ToString([Security.Cryptography.SHA1]::Create().ComputeHash([IO.File]::ReadAllBytes("$myScript"))).Replace('-','').ToLower()
+    }
+    GetFileHash = {
+        @(Get-FileHash "$myScript" -Algorithm SHA1).Hash
+    }
+    CertUtil = {
+            @(certutil.exe -hashfile "$myScript")[1]
+    }
+} -RepeatCount 100

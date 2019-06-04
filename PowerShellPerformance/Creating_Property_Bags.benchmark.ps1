bacd2722ca07beac6fdd25c1dbae7b50205056ca
StartAutomating <james.brundage@start-automating.com>
2019-06-03 19:43:23 -0700

    Adding new Benchmarks: * Creating Property Bags * Random Number Generation

---
+++ PowerShellPerformance/Creating_Property_Bags.benchmark.ps1
@@ -0,0 +1,59 @@
+﻿#requires -Module BenchPress
+
+bench -Technique @{
+    'New-Object PSObject -Property @{}' = {
+        New-Object PSObject -Property @{
+            a = 'b'
+        }
+    }
+    'New-Object | Add-Member' = { 
+        New-Object PSObject | 
+            Add-Member NoteProperty a b -PassThru
+    }
+    'PSCustomObject' = {
+        [PSCustomObject]@{
+            a='b'
+        }
+    }
+    'New-Object ; Add-Member' = {
+        $o = New-Object PSObject
+        Add-Member -InputObject $o -MemberType NoteProperty -Name a -Value b  -PassThru
+    }
+    'Static Constructors Only' = {
+        $o = [PSObject]::new()
+        $o.psobject.Properties.Add([Management.Automation.PSNoteProperty]::new('a','b'))
+        $o
+    }
+} -GroupName '(1 property)'
+
+
+bench -Technique @{
+    'New-Object PSObject -Property @{}' = {
+        New-Object PSObject -Property @{
+            a = 'b'
+            c = 'd'
+        }
+    }
+    'New-Object | Add-Member' = { 
+        New-Object PSObject | 
+            Add-Member NoteProperty a b -PassThru |
+            Add-Member NoteProperty c d -PassThru
+    }
+    'PSCustomObject' = {
+        [PSCustomObject]@{
+            a='b'
+            c='d'
+        }
+    }
+    'New-Object ; Add-Member' = {
+        $o = New-Object PSObject
+        Add-Member -InputObject $o -MemberType NoteProperty -Name a -Value b  
+        Add-Member -InputObject $o -MemberType NoteProperty -Name c -Value d  -PassThru
+    }
+    'Static Constructors Only' = {
+        $o = [PSObject]::new()
+        $o.psobject.Properties.Add([Management.Automation.PSNoteProperty]::new('a','b'))
+        $o.psobject.Properties.Add([Management.Automation.PSNoteProperty]::new('c','d'))
+        $o
+    }
+} -GroupName '(2 properties)'
---
+++ PowerShellPerformance/Random_Number_Generation.benchmark.ps1
@@ -0,0 +1,40 @@
+﻿$global:QuickRandom = [Random]::new()
+bench -Technique @{
+    'GetRandom' = {
+        Get-Random
+    }
+    '[Random]::New().Next()' = {
+        [Random]::new().Next()
+    }
+    'QuickRandom' = {
+        $global:QuickRandom.Next()        
+    }
+} -GroupName '(AnyRandomNumber)'
+
+bench -Technique @{
+    'GetRandom' = {
+        Get-Random -Minimum 10 -Maximum 100 
+    }
+    '[Random]::New().Next()' = {
+        [Random]::new().Next(10,100)
+    }
+    'QuickRandom' = {
+        $global:QuickRandom.Next(10,100)        
+    }
+} -GroupName '(Min and Max)'
+
+bench -Technique @{   
+    'GetRandom' = {
+        $buff = @(foreach ($n in 1..1kb) { Get-Random -Minimum 0 -Maximum 255}) -as [byte[]]        
+    }
+    '[Random]::New().Next()' = {
+        $buff = [byte[]]::new(1kb)
+        [random]::new().NextBytes($buff)
+        $buff = $null
+    }
+    'QuickRandom' = {
+        $buff = [byte[]]::new(1kb)
+        $global:QuickRandom.NextBytes($buff)
+        $buff = $null
+    }
+} -GroupName '(Random Byte Buffer)' -RepeatCount 10

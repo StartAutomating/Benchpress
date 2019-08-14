5ff35470d15a4b6bb5888aefed9449ce21b5d513
StartAutomating <james.brundage@start-automating.com>
2019-08-14 13:46:29 -0700

    New Performance Experiment: Sorting

---
+++ PowerShellPerformance/What_Is_The_Fastest_Way_To_Sort.benchmark.ps1
@@ -0,0 +1,14 @@
+﻿#requires -Module Benchpress
+
+$arr = 1..100 | Get-Random -Count 100
+
+
+bench -Technique @{
+    'Sort-Object' = {$arr | Sort-Object}
+    'ArrayList.Sort' = { 
+        $al = [collections.arraylist]::new($arr) 
+        $al.Sort()
+        $al
+    }
+}
+ 

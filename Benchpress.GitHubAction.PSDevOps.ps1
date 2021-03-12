d2ba38c39db403a63864bb2ec8d5eff3ef3b752f
James Brundage <james.brundage@start-automating.com>
2021-03-12 12:52:52 -0800

    Changing action branding (using watch icon)

--- Benchpress.GitHubAction.PSDevOps.ps1
+++ Benchpress.GitHubAction.PSDevOps.ps1
@@ -1,5 +1,5 @@
 ﻿#requires -Module Benchpress
 #requires -Module PSDevOps
 Import-BuildStep -ModuleName Benchpress
-New-GitHubAction -Name "Benchmark with Benchpress" -Description 'Easy Benchmarking with PowerShell' -Step Benchpress |
+New-GitHubAction -Name "Benchmark with Benchpress" -Description 'Easy Benchmarking with PowerShell' -Step Benchpress -Icon watch |
     Set-Content .\action.yml -Encoding UTF8 -PassThru

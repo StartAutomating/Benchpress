01c8fc910f1e8f2a7fb29e697b48ab8befcffa32
James Brundage <james.brundage@start-automating.com>
2021-11-07 18:01:11 -0800

    Updating Workflow Definition (including TagReleaseAndPublish)

--- Benchpress.GitHubWorkflow.PSDevOps.ps1
+++ Benchpress.GitHubWorkflow.PSDevOps.ps1
@@ -2,6 +2,5 @@
 #requires -Module PSDevOps
 Import-BuildStep -ModuleName Benchpress
 New-GitHubWorkflow -Name "StaticAnalysisAndTests" -On Demand, Push -Job PowerShellStaticAnalysis,
-    TestPowerShellOnLinux,
-    RunBenchmarks |
+    TestPowerShellOnLinux, RunBenchmarks, TagReleaseAndPublish |
     Set-Content .\.github\workflows\RunPester.yml -Encoding UTF8

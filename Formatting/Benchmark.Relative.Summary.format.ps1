f915796ac3e819047fa3722838bd42654a342dd9
James Brundage <james.brundage@start-automating.com>
2021-11-07 17:49:01 -0800

    Formatting changes: Including clockspeed (Fixes #7) and Deserializes types (Fixes #8)

---
+++ Formatting/Benchmark.Relative.Summary.format.ps1
@@ -0,0 +1,8 @@
+﻿Write-FormatView -TypeName Benchmark.Relative.Summary, Deserialized.Benchmark.Relative.Summary -Property Technique, 
+    Time, RelativeSpeed, Throughput -GroupByScript {
+        $_.FileName + ' ' + $_.GroupName + $(if ($_.ClockSpeed) { '@~'+ $_.ClockSpeed+' Mhz'})
+    } -GroupLabel 'Benchmark' -VirtualProperty @{
+        Throughput = {[Math]::Round($_.Throughput,2).ToString() + "/s"}
+        RelativeSpeed = { [Math]::Round($_.RelativeSpeed, 2).ToString() + 'x'}
+        Time = { $_.Time.ToString().Substring(0,15) }
+    }

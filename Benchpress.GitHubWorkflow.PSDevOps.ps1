857b47a073cdadb1b1cfa65ae76610682f52ea5f
James Brundage <@github.com>
2022-07-11 12:50:04 -0700

    Updating Benchpress Workflow (#15 #16)

--- Benchpress.GitHubWorkflow.PSDevOps.ps1
+++ Benchpress.GitHubWorkflow.PSDevOps.ps1
@@ -2,5 +2,5 @@
 #requires -Module PSDevOps
 Import-BuildStep -ModuleName Benchpress
 New-GitHubWorkflow -Name "StaticAnalysisAndTests" -On Demand, Push -Job PowerShellStaticAnalysis,
-    TestPowerShellOnLinux, RunBenchmarks, TagReleaseAndPublish |
+    TestPowerShellOnLinux, RunBenchmarks, TagReleaseAndPublish, HelpOut, RunEZOut |
     Set-Content .\.github\workflows\RunPester.yml -Encoding UTF8

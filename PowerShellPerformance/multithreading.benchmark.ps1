b76fbfdb4e25a15a3c8a2b34564d68e987fe527d
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-07-11 16:24:32 -0300

    renaming script

--- PowerShellPerformance/multithreading.benchmark.ps1
+++ PowerShellPerformance/multithreading.benchmark.ps1
@@ -5,43 +5,39 @@ $threads = 10
 
 # Using this to be sure no test failed
 $resultGrid = [System.Collections.Generic.List[object]]::new()
-$ran = [random]::new()
+$ran   = [random]::new()
 $items = 100000
-$testArray = while($items--)
-{
+$testArray = while($items--) {
     [pscustomobject]@{
-        FirstValue = $ran.Next()
+        FirstValue  = $ran.Next()
         SecondValue = $ran.Next()
     }
 }
 $average = [System.Linq.Enumerable]::Average(
-    [int[]]$testArray.FirstValue
+    [int[]] $testArray.FirstValue
 )
 
 $ThreadJobAvailable = Get-Module ThreadJob -ListAvailable
-$ParallelAvailable = $PSVersionTable.PSVersion -ge [version]'7.0'
+$ParallelAvailable  = $PSVersionTable.PSVersion -ge [version] '7.0'
 
-[ref]$Reference = 0
+[ref] $Reference = 0
 $groupSize = [math]::Ceiling($testArray.Count / $threads)
-$chunks = $testArray | Group-Object -Property {
+$chunks    = $testArray | Group-Object -Property {
     [math]::Floor($Reference.Value++ / $groupSize)
 }
 
 $action = {
     param($array, $average)
 
-    foreach($i in $array)
-    {
-        if($i.FirstValue -le $average -and $i.SecondValue % 2)
-        {
+    foreach($i in $array) {
+        if($i.FirstValue -le $average -and $i.SecondValue % 2) {
             $i
         }
     }
 }
 
 $expectedCount = $(
-    foreach($chunk in $chunks)
-    {
+    foreach($chunk in $chunks) {
         & $action $chunk.Group $average
     }
 ).Count
@@ -50,22 +46,21 @@ $resultObject = {
     param($TestName, $TestCount, $ExpectedCount)
 
     [pscustomobject]@{
-        Test = $TestName
-        Count = $TestCount
+        Test   = $TestName
+        Count  = $TestCount
         Status = ('FAILED', 'PASSED')[$Testcount -eq $ExpectedCount]
     }
 }
 
 $techniques = @{
     "Start-Job" = {
-        $jobs = foreach($chunk in $chunks)
-        {
+        $jobs = foreach($chunk in $chunks) {
             Start-Job -ScriptBlock $action -ArgumentList $chunk.Group, $average
         }
         $resultJob = Receive-Job $jobs -Wait -AutoRemoveJob
         $hash = @{
-            TestName = 'Job'
-            TestCount = $resultJob.Count
+            TestName      = 'Job'
+            TestCount     = $resultJob.Count
             ExpectedCount = $expectedCount
         }
         $thisTest = & $resultObject @hash
@@ -75,36 +70,28 @@ $techniques = @{
     "Runspace" = {
         $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $threads)
         $RunspacePool.Open()
-        $runspace = foreach($chunk in $chunks)
-        {
+        $runspace = foreach($chunk in $chunks) {
             $params = @{
                 array = $chunk.Group
                 average = $average
             }
             $ps = [powershell]::Create().AddScript($action).AddParameters($params)
             $ps.RunspacePool = $RunspacePool
-            
-            [pscustomobject]@{
+
+            @{
                 Instance = $ps
-                Handle = $ps.BeginInvoke()
+                Handle   = $ps.BeginInvoke()
             }
         }
-        
-        while($runspace.Handle.IsCompleted -contains $false)
-        {
-            Start-Sleep -Milliseconds 50
-        }
-        $resultRunspace = foreach($r in $runspace)
-        {
+
+        $resultRunspace = foreach($r in $runspace) {
             $r.Instance.EndInvoke($r.Handle)
-            $r.Instance.Dispose()
         }
-        
-        $runspace.Clear()
+
         $RunspacePool.Dispose()
         $hash = @{
-            TestName = 'Runspace'
-            TestCount = $resultRunspace.Count
+            TestName      = 'Runspace'
+            TestCount     = $resultRunspace.Count
             ExpectedCount = $expectedCount
         }
         $thisTest = & $resultObject @hash
@@ -112,17 +99,15 @@ $techniques = @{
     }
 }
 
-if($ThreadJobAvailable)
-{
+if($ThreadJobAvailable) {
     $techniques["Start-ThreadJob"] = {
-        $jobs = foreach($chunk in $chunks)
-        {
+        $jobs = foreach($chunk in $chunks) {
             Start-ThreadJob -ScriptBlock $action -ArgumentList $chunk.Group, $average -ThrottleLimit $threads
         }
         $resultThreadJob = Receive-Job $jobs -Wait -AutoRemoveJob
         $hash = @{
-            TestName = 'ThreadJob'
-            TestCount = $resultThreadJob.Count
+            TestName      = 'ThreadJob'
+            TestCount     = $resultThreadJob.Count
             ExpectedCount = $expectedCount
         }
         $thisTest = & $resultObject @hash
@@ -130,21 +115,18 @@ if($ThreadJobAvailable)
     }
 }
 
-if($ParallelAvailable)
-{
+if($ParallelAvailable) {
     $techniques["ForEach-Object -Parallel"] = {
         $resultParallel = $chunks | ForEach-Object -Parallel {
-            foreach($i in $_.Group)
-            {
-                if($i.FirstValue -le $using:average -and $i.SecondValue % 2)
-                {
+            foreach($i in $_.Group) {
+                if($i.FirstValue -le $using:average -and $i.SecondValue % 2) {
                     $i
                 }
             }
         } -ThrottleLimit $threads
         $hash = @{
-            TestName = 'Parallel'
-            TestCount = $resultParallel.Count
+            TestName      = 'Parallel'
+            TestCount     = $resultParallel.Count
             ExpectedCount = $expectedCount
         }
         $thisTest = & $resultObject @hash

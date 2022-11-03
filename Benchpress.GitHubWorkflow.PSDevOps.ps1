c78c903b481422f2aee67abbcac93b7b9ed6fdcf
James Brundage <@github.com>
2022-11-02 17:19:58 -0700

    Adding GitPub workflow (Fixes #33) [skip ci]

--- Benchpress.GitHubWorkflow.PSDevOps.ps1
+++ Benchpress.GitHubWorkflow.PSDevOps.ps1
@@ -9,8 +9,6 @@ New-GitHubWorkflow -Name "Build, Test, and Publish" -On Demand, Push -Job PowerS
     TestPowerShellOnLinux, TagReleaseAndPublish, BuildBenchpress -OutputPath .\.github\workflows\RunPester.yml
 
 Import-BuildStep -ModuleName GitPub
-
-Import-BuildStep -ModuleName GitPub
 New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged -OutputPath .\.github\workflows\OnIssue.yml
 
 Pop-Location
